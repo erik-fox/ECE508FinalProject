3c40b4c5c994eac6f5e5844258d8b5ba7f2406a0
Erik Fox <erfox@ruby.cecs.pdx.edu>
2021-05-18 17:30:37 -0700

    added return infectioncards to pile, and built player card deck

--- board.py
+++ board.py
@@ -5,4 +5,13 @@ class infectioncarddeck:
         self.discardpile=[]
     def playcard (self):
         self.discardpile.append(self.infectioncards.pop(random.randrange(0,len(self.infectioncards))))
-    #def Function to shuffel discard pile and place back on top
+    def return2deck(self):
+        for i in range(len(self.discardpile)):
+            self.infectioncards.append(self.discardpile.pop(random.randrange(0,len(self.discardpile))))
+
+class playercarddeck:
+    def __init__(self):
+        self.playercards =['San Francisco','Chicago', 'Montreal', 'New York', 'Washington', 'Atlanta', 'London', 'Madrid', 'Paris', 'Essen', 'Milan', 'St. Petersburg','Los Angeles', 'Mexico City', 'Miami', 'Bogota', 'Lima', 'Santiago', 'SaoPaolo', 'Buenos Aires', 'Lagos', 'Kinshasa', 'Khartoum', 'Johannesburg','Algiers', 'Istanbul', 'Cairo', 'Moscow','Baghdad','Riyadh','Tehran','Karachi','Mumbai','Delhi','Chennai','Kolkata','Bangkok','Jakarta','Beijing','Shanghai','Hong Kong','Ho Chi Minh City','Seoul', 'Taipei','Manila','Sydney','Tokyo','Osaka','Epidemic','Epidemic','Epidemic', 'Epidemic', 'Epidemic','Epidemic','Resilient Population', 'Airlift', 'Forecast','One Quiet Night', 'Government Grant']
+        self.discardpile=[]
+    def playcard(self):
+        self.discardpile.append(self.playercards.pop(random.randrange(0,len(self.playercards))))

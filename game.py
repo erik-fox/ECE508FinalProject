ce835f81703b416576bb3c705a2ddf9397284d6f
erik-fox <erikfox589@gmail.com>
2021-05-20 15:33:12 -0700

    Update game.py

--- game.py
+++ game.py
@@ -20,7 +20,7 @@ def game():
 ##play game
 ####Start with player 1
 ####display each players role, hand, map, cured, outbreak num and infection rate
-####Do 4 actions:ensure all legal actions are enabled; Give user all there legal options( limited by role, location, hand)
+####Do 4 actions:ensure all legal actions are enabled; Give user all there legal options( limited by role, location, hand); add list of research cities for shuttle flights
 ####draw 2 player cards
 ####infect cities
 #### check if game is over before going to next player

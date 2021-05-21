f454e1d65a5285c3f7bde78a6e91a4bc15a24e91
Erik Fox <erfox@ruby.cecs.pdx.edu>
2021-05-20 20:09:49 -0700

    working

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ def game():
     b.researchstations=5
 
 ##select role
-    numplayers=int(input('How many players?'))
+    numplayers=int(input('How many players? 2-4'))
     players=[]
     for i in range (numplayers):
         for j in range(len(b.roles)):
@@ -16,6 +16,22 @@ def game():
         roleselect=int(input('Enter number of role'))
         players.append(player(b.roles.pop(roleselect),b.map.list['atlanta']))
 ##distribute non epidemic player cards based on num of players
+    if numplayers == 2:
+        numcards=4
+    elif numplayers ==3:
+        numcards=3
+    else:
+        numcards=2
+    for j in range(numplayers):
+        for i in range(numcards):
+            while True:
+                b.playerdeck.playcard()
+                if b.playerdeck.discardpile[len(b.playerdeck.discardpile)-1]=='Epidemic':
+                    b.playerdeck.playercards.append(b.playerdeck.discardpile[len(b.playerdeck.discardpile)-1])
+                else:
+                    players[j].hand.append(b.playerdeck.discardpile.pop())
+                    break
+            print(players[j].hand)
 ##infect 9 cities
 ##play game
 ####Start with player 1
